0f83f4b3f600eed4f45932d84c5a0023a7a44f18
Valentin Piho <pibox.ee@gmail.com>
2020-03-23 16:10:31 +0200

    Updated

--- src/MethodsOverload.java
+++ src/MethodsOverload.java
@@ -1,22 +1,26 @@
 public class MethodsOverload {
     public static void main(String[] args) {
 
-    double result = calcFeetAndInchesToCentimeters(43423);
-        System.out.println(result);
+    double result = calcFeetAndInchesToCentimeters(43423, 32);
     }
 
     public static double calcFeetAndInchesToCentimeters(double feet, double inches){
         if ((feet >= 0) && (inches >= 0 && inches <= 12)) {
-            return (feet * 12 + inches) * 2.54;
+            double cm = (feet * 12 + inches) * 2.54;
+            System.out.println(feet + " feet and " + inches + " inches = " + cm + " centimeters.");
+            return cm;
+        } else if (feet >= 0 && inches >= 0) {
+            double onlyInches = feet * 12 + inches;
+            return calcFeetAndInchesToCentimeters(onlyInches);
         }
         return -1;
     }
 
     public static double calcFeetAndInchesToCentimeters(double inches){
         if (inches >= 0) {
-            double feet = inches / 12;
-            double newInches = inches - feet * 12;
-            return calcFeetAndInchesToCentimeters(feet, newInches);
+            double feet = (int) inches / 12;
+            double remainingInches = (int) inches % 12;
+            return calcFeetAndInchesToCentimeters(feet, remainingInches);
         }
         return -1;
     }
